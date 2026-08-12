6184742624f27d329431de6a2ff3e6b2eded7696
Asem Hamid <197576625+as-em@users.noreply.github.com>
2026-08-12 14:09:10 +0600

    Create try1.cpp

---
+++ Lab-1-(12-08-2026)/try1.cpp
@@ -0,0 +1,16 @@
+#include <iostream> // # include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+
+    cout << "Hello" << endl;
+    int a, c;
+
+    cin >> a >> c;
+
+    cout << a << " "
+         << c << endl;
+    return 0;
+}
